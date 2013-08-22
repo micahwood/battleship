f9689b300c362464776d37e4728b0a6b23a7b743
Bryce Hill <bryce@brycehill.net>
2013-08-22 14:27:33 -0700

    began adding Passport for authentication

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,7 @@
 // Routes for /login
+// 
+var passport = require('passport');
+
 app.get('/login', function(req, res) {
   var locals = {
     title : 'Battleship :: Login',
@@ -8,9 +11,11 @@ app.get('/login', function(req, res) {
   res.render('login.jade', locals);
 });
 
-app.post('/login', function(req, res) {
-
-});
-
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/account',
+    failureRedirect: '/login',
+    failureFlash: true
+  })
+);
 
 
--- server.js
+++ server.js
@@ -3,81 +3,104 @@ var express = require('express'),
     connect = require('connect'),
     io      = require('socket.io'),
     fs      = require('fs'),
+    passport      = require('passport'),
+    LocalStrategy = require('passport-local').Strategy,
     port    = (process.env.PORT || 8081);
 
 // Global - but shouldn't be 
 app = express();
 require('./routes/');
 
-
 app.set('title', 'Battleship');
 
-// Setup Express
+/*
+ * Setup Express
+ */ 
 app.configure(function() {
-    app.set('views', __dirname + '/views');
-    app.set('view options', { layout: false });
-    app.use(connect.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.session({ secret: "shhhhhhhhh!"}));
-    app.use(connect.static(__dirname + '/public'));
-    app.use(app.router);
+  app.set('views', __dirname + '/views');
+  app.set('view options', { layout: false });
+  app.use(connect.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'MY$UPERSECRETKEY'}));
+  app.use(connect.static(__dirname + '/public'));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
 });
 
 
+/**
+ * Setup Passport
+ */
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({username: username}, function(err, user) {
+      if (err) return done(err);
+
+      if (!user || !user.validPassword(password)) {
+        return done(null, false, { message: "Incorrect username or password." });
+      }
+
+      return done(null, user);
+    });
+  }
+));
+
 // Version 3.x of Express needs this server param
 var server = http.createServer(app);
+server.listen(port);
+
 var users = [];
 
 // Routes
 
 
-// Setup Socket.IO
-var io = io.listen(server);
-io.sockets.on('connection', function(socket){
+// // Setup Socket.IO
+// var io = io.listen(server);
+// io.sockets.on('connection', function(socket){
   
-  console.log('Client Connected');
+//   console.log('Client Connected');
 
-  socket.on('joinRoom', function(name) {
-console.log('server: joining room')
-console.log(name)
-    var room = getRoomID(); // or something like this?
-    m
-    socket.name = name; 
-    socket.room = room
-    users[name] = name; 
-    socket.join(room);
+//   socket.on('joinRoom', function(name) {
+// console.log('server: joining room')
+// console.log(name)
+//     var room = getRoomID(); // or something like this?
+//     m
+//     socket.name = name; 
+//     socket.room = room
+//     users[name] = name; 
+//     socket.join(room);
 
-    // redirect to game page. 
+//     // redirect to game page. 
 
-    // socket.emit('updatechat', 'SERVER', 'you have connected to room1');
-    // // echo to room 1 that a person has connected to their room
-    // socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
-    // socket.emit('updaterooms', rooms, 'room1');
-  });
+//     // socket.emit('updatechat', 'SERVER', 'you have connected to room1');
+//     // // echo to room 1 that a person has connected to their room
+//     // socket.broadcast.to('room1').emit('updatechat', 'SERVER', username + ' has connected to this room');
+//     // socket.emit('updaterooms', rooms, 'room1');
+//   });
 
-  socket.on('message', function(data){
-    socket.broadcast.emit('server_message',data);
-    socket.emit('server_message',data);
-  });
+//   socket.on('message', function(data){
+//     socket.broadcast.emit('server_message',data);
+//     socket.emit('server_message',data);
+//   });
   
-  socket.on('disconnect', function(){
-    console.log('Client Disconnected.');
-  });
-});
-
-server.listen(port);
+//   socket.on('disconnect', function(){
+//     console.log('Client Disconnected.');
+//   });
+// });
 
 
 
-// Suppose these rooms could get stored somewhere eventually?
-var rooms = []; 
 
-function getRoomID() {
-  console.log('getRoomID')
+// // Suppose these rooms could get stored somewhere eventually?
+// var rooms = []; 
+
+// function getRoomID() {
+//   console.log('getRoomID')
 
 
-  return id; 
-};
+//   return id; 
+// };
 
 
 
