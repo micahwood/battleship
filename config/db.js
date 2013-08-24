0c99c0542bc310c3e0cf4f819a0366e299b1c3e8
Bryce Hill <bahill@godaddy.com>
2013-08-24 12:15:23 -0700

    export mongoose model, not schema

--- config/db.js
+++ config/db.js
@@ -2,13 +2,16 @@ var mongoose = require('mongoose'),
     fs       = require('fs');
 
 // loop through models and register them with mongoose. 
-fs.readdir('./models', function(err, files) {
-  files.forEach(function(file) {
-    var name = file.replace('.js', '');
+// fs.readdir('./models', function(err, files) {
+//   files.forEach(function(file) {
+//     var name = file.replace('.js', '');
 
-    schema = require('../models/' + file);
-    mongoose.model(name, schema);
-  });
-});
+//     schema = require('../models/' + file);
+//     mongoose.model(name, schema);
+//   });
+
+//   // I don't like this here, but for now...
+//   require('./auth');
+// });
 
 mongoose.connect('mongodb://localhost/battleship');
