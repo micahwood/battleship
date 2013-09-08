3f92963a0973c98807b4cbd77a61a9c4a4597416
Bryce Hill <bryce@brycehill.net>
2013-09-07 20:55:24 -0700

    added more config options

--- config/index.js
+++ config/index.js
@@ -1,2 +1,30 @@
 require('./db');
 require('./auth');
+
+var config = {
+  local: {
+    mode: 'local',
+    host: 'localhost',
+    port: 8081
+  },
+  tiltjuice: {
+    mode: 'tilejuice',
+    host: 'http://tiltjuice.com',
+    port: 8081
+  },
+  production: {
+    mode: 'production',
+    port: 8081
+  },
+
+  mongo: {
+    host: 'mongodb://localhost/battleship',
+    port: 27017
+  }
+};
+
+module.exports = function(mode) {
+  return config[mode || process.argv[2] || 'local'] || config.local;
+
+  // Could return different mongo configurations here too?
+};
