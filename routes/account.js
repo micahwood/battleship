32679ff369542d3dd4fc031d4fe97eaa29f2dc0f
Bryce Hill <bryce@brycehill.net>
2013-09-12 23:02:18 -0700

    add check for current games user is playing

--- routes/account.js
+++ routes/account.js
@@ -1,3 +1,5 @@
+var Game = require('../models/Game');
+
 // Simple route middleware to ensure user is authenticated.
 //   Use this route middleware on any resource that needs to be protected.  If
 //   the request is authenticated (typically via a persistent login session),
@@ -11,13 +13,21 @@ var ensureAuthenticated = function(req, res, next) {
 }
 
 app.get('/account', ensureAuthenticated, function(req, res){
-  var locals = {
-    title : 'Battleship :: Account',
-    description: 'This page has your account information for your Battleship user.',
-    user: req.user.username
-  };
+  var user = req.user.username;
 
-  res.render('account', locals);
+  // Will eventually want to modify this query 
+  Game.find({'users.username': user}, 'gid', function(err, game) {
+    if (err) return err;
+
+    var locals = {
+      title : 'Battleship :: Account',
+      description: 'This page has your account information for your Battleship user.',
+      user: req.user.username,
+      game: game
+    };
+
+    res.render('account', locals);
+  });
 });
 
 exports.auth = ensureAuthenticated;
