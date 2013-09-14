e8690e9f991d91655619074d4f98ece8d80672ae
Bryce Hill <bryce@brycehill.net>
2013-09-14 15:45:31 -0700

    moving to rooms from namespaces

--- sockets.js
+++ sockets.js
@@ -3,46 +3,27 @@ var socketio = require('socket.io');
 module.exports.listen = function(app) {
   io = socketio.listen(app);
 
-  
-  var users = {};
-  //too lazy to loop through users object to get a count
-  var count = 0;
-
-  var join = io.of('/join');
-  console.log('listening.......');
-  join.on('connection', function(socket){
+  // Going to try to use rooms instead of namespaces 
+  // since the room names are created dynamically. 
+  io.sockets.on('connection', function(socket){
     console.log('Client Connected');
 
-    socket.on('addUser', function(user) {
-      count++;
-      socket.me = user;
-      console.log(user);
+    socket.on('joinGame', function(data) {
+      var room = data.gid,
+          user = data.username;
+      console.log(user + ' is joining room: ' + room);
+      socket.room = room;
+      socket.user = user;
+      socket.join(room);
 
-      if (count >= 2) {
-        opponent = '';
-        for (var player in users) {
-          opponent = player;
-        }
-        socket.emit("gameStart", opponent);
-        socket.broadcast.emit("gameStart", socket.me);
-      }
-      users[user] = user;
+      //   // redirect to game page. 
+      io.sockets.clients(room);
     });
 
-    /*
-     *  
-     */
-    // socket.on('joinGame', function(name) {
-    //   console.log('server: joining room');
-    //   console.log(name);
-    //   var room = 'room';//getRoomID(); // or something like this?
-
-    //   socket.name = name;
-    //   socket.room = room;
-    //   // users[name] = name;
-    //   socket.join(room);
-
-    //   // redirect to game page. 
+    socket.on('disconnect', function(){
+      console.log('Client Disconnected.');
+    });
+  });
 
     //   // socket.emit('updatechat', 'SERVER', 'you have connected to room1');
     //   // // echo to room 1 that a person has connected to their room
@@ -55,12 +36,5 @@ module.exports.listen = function(app) {
     //   socket.emit('server_message',data);
     // });
 
-    socket.on('disconnect', function(){
-      console.log('Client Disconnected.');
-      delete users[socket.me];
-      count--;
-    });
-  });
-
   return io;
-}
+};
