5e947c51a1124546d38e31cf5aed99e4cab9f4d3
brycehill <bryce@brycehill.net>
2013-10-11 17:27:46 -0700

    remove unneeded files

--- routes/main.js
+++
@@ -1,8 +0,0 @@
-app.get('/', function(req, res){
-  var locals = {
-    title : 'Battleship :: Home',
-    description: 'This page is about Battleship and how it rocks.'
-  };
-
-  res.render('main.jade', locals);
-});
