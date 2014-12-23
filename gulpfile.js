e9b5ae0a5f1741001463b29c61f2d1b219d4be85
Bryce Hill <bryceahill@gmail.com>
2014-12-22 17:34:58 -0700

    Add lint task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,46 @@
 var gulp = require('gulp')
 var nodemon = require('gulp-nodemon')
-var jshint = require('gulp-jshint')
+var lint = require('gulp-jshint')
 var refresh = require('gulp-livereload')
+
+var paths = {
+    scripts: [
+        './client/js/*.js',
+        './client/js/models/*.js',
+        './client/js/routes/*.js',
+        './client/js/views/*.js',
+    ],
+    styles: [
+        './client/css/**/*.css'
+    ],
+    server: {
+
+    }
+}
+
+gulp.task('scripts', function() {
+    gulp.src(paths.scripts)
+        .pipe(lint())
+        .pipe(lint.reporter('jshint-stylish'));
+})
 
 gulp.task('lint', function() {
     gulp.src('./**/*.js')
-        .pipe(jshint())
+        .pipe(lint())
+        .pipe(lint.reporter('jshint-stylish'));
 })
 
 gulp.task('serve', function () {
     // refresh.listen();
     nodemon({ script: 'server.js' })
-        .on('change', 'lint')
+        // .on('change', ['lint'])
     // gulp.src('./server.js')
     //     .pipe(refresh())
 });
 
 gulp.task('watch', function() {
-    gulp.watch(['./**/*.js'])
+    gulp.watch(paths.scripts, ['scripts'])
 })
 
 
-gulp.task('develop', ['lint', 'serve'])
+gulp.task('develop', ['watch'])
